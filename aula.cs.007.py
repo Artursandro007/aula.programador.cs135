13272485b8882f6f738c8e419a150cd7984e8ad5
Artursandro007 <166613413+Artursandro007@users.noreply.github.com>
2024-04-10 08:46:26 -0300

    Add files via upload

---
+++ aula.cs.007.py
@@ -0,0 +1,17 @@
+print (" bom dia ")
+# calcule o lados do triangolo
+a = float(input('escreva o tamanho do primeira lado '))
+b = float(input('escreva o tamanho do segundo lado '))
+c = float(input('escreva o tamano do terceiro lado '))
+   
+
+            # tipos de triangulos
+if(a + b < c) or ( a + c < b)or(b + c < a):
+   print("nao e um triangulo") 
+elif (a == b) and (a == c):
+   print(" equilatero")
+elif (a == b) or (a == c) or (b == c):
+   print ("isosceles")
+else:
+   print ("escaleno")
+   
